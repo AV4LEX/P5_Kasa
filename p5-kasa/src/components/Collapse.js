61a32697c83a0f7dc8bc3ebc38535b6ce55009d6
alex.vaslin <vaslinalex@gmail.com>
2024-06-24 14:41:16 +0200

    css page logement

--- p5-kasa/src/components/Collapse.js
+++ p5-kasa/src/components/Collapse.js
@@ -1,26 +1,27 @@
-import React, {useState} from "react";
-import "../styles/Collapse.scss"
-import Arrow from "../assets/images/arrow.svg"
+import React, { useState } from "react";
+import "../styles/Collapse.scss";
+import Arrow from "../assets/images/arrow.svg";
 
-function Collapse({title, description}) {
-    //creat a state Hook
+function Collapse({ title, description }) {
     const [open, setOpen] = useState(false);
 
-    return(
-        <div className="collapse" id={`collapse-${title}`}>
+    return (
+        <div className={`collapse ${open ? 'open' : ''}`}>
             <div className="collapse-header">
                 <div className="collapse-title">{title}</div>
-                <span className={`collapse-arrow ${open}`}>
-                    <img src={Arrow} alt="ouvrir l'onglet" className="collapse-arrow-img"  onClick={() => setOpen(!open)}></img>
+                <span className={`collapse-arrow ${open ? 'true' : ''}`}>
+                    <img
+                        src={Arrow}
+                        alt="ouvrir l'onglet"
+                        className="collapse-arrow-img"
+                        onClick={() => setOpen(!open)}
+                    />
                 </span>
             </div>
 
-            {
-                open && <div className="collapse-description">{description}</div>
-            }
-
+            {open && <div className="collapse-description">{description}</div>}
         </div>
     );
-} 
+}
 
 export default Collapse;
