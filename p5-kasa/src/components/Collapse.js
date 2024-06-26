34ae8ba2586b7a49e9da1515317fea47af11202b
alex.vaslin <vaslinalex@gmail.com>
2024-06-26 15:44:51 +0200

    responsive ajustement et animations collapses

--- p5-kasa/src/components/Collapse.js
+++ p5-kasa/src/components/Collapse.js
@@ -3,25 +3,30 @@ import "../styles/Collapse.scss";
 import Arrow from "../assets/images/arrow.svg";
 
 function Collapse({ title, description }) {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    return (
-        <div className={`collapse ${open ? 'open' : ''}`}>
-            <div className="collapse-header">
-                <div className="collapse-title">{title}</div>
-                <span className={`collapse-arrow ${open ? 'true' : ''}`}>
-                    <img
-                        src={Arrow}
-                        alt="ouvrir l'onglet"
-                        className="collapse-arrow-img"
-                        onClick={() => setOpen(!open)}
-                    />
-                </span>
-            </div>
+  const TOGGLE_COLLAPSE = () => {
+    setOpen(!open);
+  };
 
-            {open && <div className="collapse-description">{description}</div>}
-        </div>
-    );
+  return (
+    <div className={`collapse ${open ? "open" : ""}`}>
+      <div className="collapse-header" onClick={TOGGLE_COLLAPSE}>
+        <div className="collapse-title">{title}</div>
+        <span className={`collapse-arrow ${open ? "true" : ""}`}>
+          <img
+            src={Arrow}
+            alt="ouvrir l'onglet"
+            className="collapse-arrow-img"
+          />
+        </span>
+      </div>
+
+      <div className="collapse-content" style={{ maxHeight: open ? "1000px" : "0" }}>
+        <div className="collapse-description">{description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Collapse;
